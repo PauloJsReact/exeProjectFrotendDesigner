d0553865a981bb7a01a4943e6db4045743f7b9eb
PauloJsReact <paulo_henrique_araujo@outlook.com>
2021-12-12 18:59:51 -0300

    An animation has been created in preloading using css and to add a function to deasappear this window

--- HTML_CSS_JS/js/main.js
+++ HTML_CSS_JS/js/main.js
@@ -1,5 +1,17 @@
+//Declarando variaveis
 var btnContact = document.querySelector(".jl-btn-contact");
 
+//Page preloader
+window.addEventListener("load", function () {
+    var pagePreloader = this.document.querySelector(".jl-preloader");
+    pagePreloader.classList.add("jl-fade-out");
+
+    setTimeout(function () {
+        pagePreloader.style.display = "none";
+    }, 2000)
+})
+
+//Declarando informaçoes de contato
 btnContact.addEventListener("click", function () {
     var boxContact = document.querySelector(".jl-contact-info");
     boxContact.classList.toggle("jl-is-open");
