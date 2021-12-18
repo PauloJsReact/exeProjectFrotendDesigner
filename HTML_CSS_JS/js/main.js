eb2870341ec2b09a3e81872b2dfcb1d49345e14b
PauloJsReact <paulo_henrique_araujo@outlook.com>
2021-12-18 19:28:44 -0300

    The function cubic besier as add to project

--- HTML_CSS_JS/js/main.js
+++ HTML_CSS_JS/js/main.js
@@ -1,5 +1,6 @@
 //Declarando variaveis
 var btnContact = document.querySelector(".jl-btn-contact");
+var toggleModal = document.querySelectorAll(".jl-toggle-modal");
 
 //Page preloader
 window.addEventListener("load", function () {
@@ -18,3 +19,18 @@ btnContact.addEventListener("click", function () {
 
     this.classList.toggle("jl-change-ico");
 })
+
+//  Abrindo e fechando o modal  Orçammento 
+
+for (let i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener("click", function () {
+        var overlay = document.querySelector(".jl-overlay");
+        var modalOrcamento = document.querySelector("#jl-modal-orcamento");
+
+        overlay.classList.toggle("jl-is-open");
+        modalOrcamento.classList.toggle("jl-is-open");
+        modalOrcamento.classList.toggle("jl-slaid-top-in");
+
+    })
+
+}
